04b4bed9a327e80a8beb0f419b9d63e22121efac
thespacedoctor <nothingbutdave@gmail.com>
2016-05-27 16:47:31 +0100

    added a class to report current mjd

--- astrocalc/cl_utils.py
+++ astrocalc/cl_utils.py
@@ -8,11 +8,13 @@ Usage:
     astrocalc sep <ra1> <dec1> <ra2> <dec2>
     astrocalc timeflip <datetime>
     astrocalc trans <ra> <dec> <north> <east>
+    astrocalc now mjd
 
     coordflip             flip coordinates between decimal degrees and sexegesimal and vice-versa
     sep                   calculate the separation between two locations in the sky.
     timeflip              flip time between UT and MJD. <datetime> within in MJD or UT
     trans                 translate a location across the sky (north and east in arcsec)
+    now                   report current time in various formats
     -h, --help            show this help message
 
 """
@@ -208,6 +210,13 @@ def main(arguments=None):
         )
 
         print "%(newRa)s, %(newDec)s (%(ra)s, %(dec)s)" % locals()
+
+    if now:
+        from astrocalc.times import now
+        mjd = now(
+            log=log
+        ).get_mjd()
+        print mjd
 
     if "dbConn" in locals() and dbConn:
         dbConn.commit()
