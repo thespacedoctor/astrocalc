932219aebef673e8d7fa44ca04ad4ad1f89b94ed
Dave <d.r.young@qub.ac.uk>
2021-03-15 12:26:54 +0000

    x

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os
 moduleDirectory = os.path.dirname(os.path.realpath(__file__))
 exec(open(moduleDirectory + "/astrocalc/__version__.py").read())
 
+
 def readme():
     with open(moduleDirectory + '/README.md') as f:
         return f.read()
@@ -34,7 +35,6 @@ setup(name="astrocalc",
           'Development Status :: 4 - Beta',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 2.7',
           'Topic :: Utilities',
       ],
       keywords=['astronomy, calculate, convert'],
